e47f5712c2c26ce46a47b331061dc5ef3b31fcb6
Trabelsiala <93924716+Trabelsiala@users.noreply.github.com>
2023-04-15 03:04:07 +0100

    crud+fxml

--- src/Services/ISC.java
+++ src/Services/ISC.java
@@ -9,6 +9,15 @@ package Services;
  *
  * @author Ala
  */
-public interface ISC {
+import java.util.List;
+/**
+ *
+ * @author Ala
+ */
+public interface ISC<T> {
+    public List<T> affichercat();
+    public void ajoutercat(T a);
+    public void modifercat(T a);
+    public void supprimercat(int id);
     
 }
